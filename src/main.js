d575f4ab611fa1219f9ed75baf8c1e22d46258d0
xuewa <366653992@qq.com>
2017-07-25 17:28:05 +0800

    header.vue init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import '../src/common/stylus/index.styl'
+import '@/common/stylus/index.styl'
+import fastclick from 'fastclick'
 
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
